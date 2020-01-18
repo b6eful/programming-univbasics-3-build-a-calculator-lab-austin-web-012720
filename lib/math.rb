724117248eafc0d0770596bbbc59fce8bba553fb
Phoenix R <phoenixrabang@gmail.com>
2020-01-18 20:26:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,5 @@
 def addition(num1, num2)
-  puts num1 + num2
+  puts (num1 + num2)
 end
 
 def subtraction(num1, num2)
@@ -7,7 +7,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  puts num1 / num2
+  puts (num1 / num2)
 
 end
 
